a50fcb1898d5b03ecaf345d4b63ac60150da449b
rymnc <43716372+rymnc@users.noreply.github.com>
2024-03-14 11:14:16 +0530

    fix: cleanup

--- rln/utils.go
+++ rln/utils.go
@@ -1,17 +1,10 @@
 package rln
 
 import (
-	"testing"
-
-	"github.com/consensys/gnark-crypto/ecc"
 	bls12_377Fr "github.com/consensys/gnark-crypto/ecc/bls12-377/fr"
 	bn254Fr "github.com/consensys/gnark-crypto/ecc/bn254/fr"
 	"github.com/consensys/gnark/frontend"
-
-	"github.com/consensys/gnark/test"
 )
-
-// TODO: get kats for other curves
 
 func GetBn254PathElements() [20]frontend.Variable {
 	rawPathElements := [20]string{
@@ -141,67 +134,4 @@ func GetBls12_377Nullifier() frontend.Variable {
 
 func GetBls12_377Y() frontend.Variable {
 	return Bls12_377FrFromStr("1363155474730118727426662020141552125273305162602216677592749750619552418376")
-}
-
-func TestRlnCircuit(t *testing.T) {
-	assert := test.NewAssert(t)
-
-	var rlnCircuit RlnCircuit
-	identityPathIndex := [20]frontend.Variable{
-		1,
-		1,
-		1,
-		0,
-		1,
-		0,
-		1,
-		0,
-		1,
-		0,
-		1,
-		0,
-		0,
-		0,
-		0,
-		0,
-		1,
-		1,
-		1,
-		0,
-	}
-
-	curves := ecc.Implemented()
-	for _, curve := range curves {
-		switch curve {
-		case ecc.BN254:
-			assert.ProverSucceeded(&rlnCircuit, &RlnCircuit{
-				X:                 frontend.Variable(GetBn254X()),
-				ExternalNullifier: frontend.Variable(GetBn254ExternalNullifier()),
-				IdentitySecret:    frontend.Variable(GetBn254IdentitySecret()),
-				MessageId:         frontend.Variable(1),
-				UserMessageLimit:  frontend.Variable(100),
-				PathElements:      GetBn254PathElements(),
-				IdentityPathIndex: identityPathIndex,
-				Y:                 frontend.Variable(GetBn254Y()),
-				Root:              frontend.Variable(GetBn254Root()),
-				Nullifier:         frontend.Variable(GetBn254Nullifier()),
-			}, test.WithCurves(ecc.BN254))
-		case ecc.BLS12_377:
-			assert.ProverSucceeded(&rlnCircuit, &RlnCircuit{
-				X:                 frontend.Variable(GetBls12_377X()),
-				ExternalNullifier: frontend.Variable(GetBls12_377ExternalNullifier()),
-				IdentitySecret:    frontend.Variable(GetBls12_377IdentitySecret()),
-				MessageId:         frontend.Variable(1),
-				UserMessageLimit:  frontend.Variable(100),
-				PathElements:      GetBls12_377PathElements(),
-				IdentityPathIndex: identityPathIndex,
-				Y:                 frontend.Variable(GetBls12_377Y()),
-				Root:              frontend.Variable(GetBls12_377Root()),
-				Nullifier:         frontend.Variable(GetBls12_377Nullifier()),
-			}, test.WithCurves(ecc.BLS12_377))
-		default:
-			continue
-		}
-	}
-
 }
